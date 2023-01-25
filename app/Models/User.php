cedff735a73a44137ce2a340bc79d468a2cf5e95
Damilare Oluwole <doluwole@parkway.ng>
2023-01-25 02:38:36 +0100

    credit webhook added

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable
 
     public function transactions()
     {
-        return $this->hasMany(Transaction::class);
+        return $this->hasMany(Transaction::class)->latest();
     }
 }
