724b3a84f71d946c4173148e5fe6c6971f6ac582
Damilare Oluwole <doluwole@parkway.ng>
2023-01-26 16:25:26 +0100

    fix

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {Route::prefix('in')->group(function () {
